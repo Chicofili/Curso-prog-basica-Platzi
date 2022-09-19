345751ffe54392e4c41af086ad12e75d92376684
Chicofili <paralelogramo17@gmail.com>
2022-09-19 17:52:43 -0300

    Se puede elegir mascota de jugador y enemigo

--- Primer proyecto/mokepon/js/mokepon.js
+++ Primer proyecto/mokepon/js/mokepon.js
@@ -8,16 +8,36 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatihueya = document.getElementById('ratihueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if (inputHipodoge.checked) {
-        alert('Seleccionaste a Hipodogue')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked) {
-        alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatihueya.checked) {
-        alert('Seleccionaste a Ratihueya')
+        spanMascotaJugador.innerHTML = 'Ratihueya'
     } else {
         alert('Elige a tu mascota!')
     }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratihueya'
+    }
+}
+
+function aleatorio(Min,Max){
+    return Math.floor(Math.random() * (Max - Min + 1) + Min)
 }
 
 //este addeventlistener ejecutara la funcion iniciar juego solo cualdo cargue el html
